697df9a16a121316ce398e81aff836c80dda89e2
Adam Gospodarczyk <adam@overment.com>
2018-06-05 00:08:30 +0200

    Initial commit

---
+++ start/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { join } from 'path';
+import config from './config/config';
+import { notFound, catchErrors } from './middlewares/errors';
+import bodyParser from 'body-parser';
+import register from 'babel-core/register';
+import babelPolyfill from 'babel-polyfill';
+
+// Connect to database
+import dbConfig from './config/database';
+import mongoose from 'mongoose';
+
+mongoose.connect(dbConfig.mongoUrl);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes config
+// ...
+
+// errors handling
+app.use(notFound);
+app.use(catchErrors);
+
+// let's play!
+app.listen(config.server.port, () => {
+    console.log(`Server is up!`);
+});
